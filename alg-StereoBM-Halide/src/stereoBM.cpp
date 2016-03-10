e134365e4c0fc95e1eac7291ecdc8881575c3f29
mzhang94 <mzhang94@mit.edu>
2016-03-10 03:19:51 -0500

    halide implementation w/o schedule

--- alg-StereoBM-Halide/src/stereoBM.cpp
+++ alg-StereoBM-Halide/src/stereoBM.cpp
@@ -1,6 +1,7 @@
 #include "Halide.h"
 #include <limits>
 #include "halide_image_io.h"
+#include "imageLib.h"
 
 using namespace Halide;
 
@@ -11,90 +12,111 @@ void apply_default_schedule(Func F) {
     flist.insert(std::make_pair(F.name(), F.function()));
     std::map<std::string,Internal::Function>::iterator fit;
     for (fit=flist.begin(); fit!=flist.end(); fit++) {
-        Func f(fit->second);
-        f.compute_root();
-        std::cout << "Warning: applying default schedule to " << f.name() << std::endl;
+        // Func f(fit->second);
+        Internal::Function f = fit->second;
+        Func wrapper(f);
+        wrapper.compute_root();
+        // if (f.schedule().compute_level().is_inline()){
+        //     std::cout << "Warning: applying inline schedule to " << f.name() << std::endl;
+        // }
     }
     std::cout << std::endl;
 }
 
-Func prefilterNorm(Func image, int winsize, int w, int h) {
+Func prefilterXSobel(Func image, int w, int h) {
     Var x("x"), y("y"), c("c");
-    Func intensity("intensity");
-    intensity(x, y) = (image(x, y, 0) + image(x, y, 1) + image(x, y, 2))/3.0f;
     Func clamped("clamped");
-    clamped(x, y) = intensity(clamp(x, 0, w-1), clamp(y, 0, h-1));
+    clamped(x, y, c) = image(clamp(x, 0, w-1), clamp(y, 0, h-1), c);
 
-    Func vsum("vsum"), hsum("sum");
-    int win2 = winsize/2;
-    RDom rw(-win2, win2);
-    vsum(x, y) = 0.0f;
-    RDom rx(0, w), ry(0, h);
-    vsum(x, ry) = select(ry == 0, sum(clamped(x, rw)), vsum(x, ry-1) - clamped(x, ry-win2-1) + clamped(x, ry + win2));
+    Func temp("temp"), xSobel("xSobel");
+    temp(x, y, c) = clamped(x+1, y, c) - clamped(x-1, y, c);
+    xSobel(x, y, c) = clamp(temp(x, y-1, c) + 2 * temp(x, y, c) + temp(x, y+1, c), -31, 31);
 
-    hsum(x, y) = 0.0f;
-    hsum(rx, y) = select(rx == 0, sum(vsum(rw, y)), hsum(rx-1, y) - vsum(rx-win2-1, y) + vsum(rx + win2, y));
-
-    Func factor("factor");
-    Expr x_size = min(w - x + win2, min(x + win2, winsize));
-    Expr y_size = min(h - y + win2, min(y + win2, winsize));
-    factor(x, y, c) = hsum(x,y) / (x_size * y_size)/ hsum(x, y);
-    Func filtered("filtered");
-    filtered(x, y, c) = image(x, y, c) / factor(x, y, c);
-    return filtered;
+    // Schedule
+    // Var xi("xi"), xo("xo"), yi("yi"), yo("yo");
+    // xSobel.compute_root();
+    // xSobel.tile(x, y, xo, yo, xi, yi, 128, 32);
+    // temp.compute_at(xSobel, yi).vectorize(x, 16);
+    // xSobel.parallel(yo);
+    return xSobel;
 }
 
 Func findStereoCorrespondence(Func left, Func right, int SADWindowSize, int minDisparity, int numDisparities,
-    int xmin, int xmax, int ymin, int ymax, float uniquenessRatio = 0.15) {
-    Func cost("cost"), diff("diff");
-    Var x("x"), y("y"), c("c"), d("d");
+    int xmin, int xmax, int ymin, int ymax, float uniquenessRatio = 0.15, int disp12MaxDiff = 1) {
 
-    diff(x, y, d) = abs(left(x, y, 0) - right(x+d, y, 0)) + abs(left(x, y, 1) - right(x+d, y, 1)) + abs(left(x, y, 2) - right(x+d, y, 2));
-    int w2 = SADWindowSize / 2;
-    RDom r(-w2, w2);
-    Func vsum("vsum");
-    RDom rx(xmin, xmax-xmin+1), ry(ymin, ymax-ymin+1);
-    vsum(x, y, d) = 0.0f;
-    vsum(x, ry, d) = select(ry == ymin, sum(diff(x, r, d)), vsum(x, ry-1, d) - diff(x, ry-w2-1, d) + diff(x, ry+w2, d));
-    cost(x, y, d) = 0.0f;
-    cost(rx, y, d) = select(rx == xmin, sum(vsum(r, y, d)), cost(rx-1, y, d) - vsum(rx-w2-1, y, d) + vsum(rx+w2, y, d));
+    Var x("x"), y("y"), c("c"), d("d");
+    Func diff("diff");
+    diff(x, y, d) = abs(left(x, y, 0) - right(x-d, y, 0)) + abs(left(x, y, 1) - right(x-d, y, 1)) + abs(left(x, y, 2) - right(x-d, y, 2));
+
+    Func cSAD("cSAD"), vsum("vsum");
+    RDom r(-SADWindowSize/2, SADWindowSize);
+    vsum(x, y, d) = sum(cast<int>(diff(x + r, y, d)));
+    cSAD(x, y, d) = sum(vsum(x, y + r, d));
 
     RDom rd(minDisparity, numDisparities);
-    Func minsad("minsad");
-    //  second_min_sad, second_min_cost, minsad, mincost,
-    minsad(x, y) = Tuple(FILTERED, std::numeric_limits<float>::infinity(), minDisparity, cost(x, y, minDisparity));
-    Expr compareMin = cost(x,y,rd) < minsad(x,y)[3];
-    Expr compareSecondMin = cost(x,y,rd) < minsad(x,y)[2];
-    minsad(x, y) = tuple_select({compareMin, compareMin, compareMin, compareMin},
-                                {minsad(x,y)[0], minsad(x,y)[1], rd, cost(x,y,rd)},
-                                {select(compareSecondMin, rd, minsad(x,y)[2]),
-                                 select(compareSecondMin, cost(x,y,rd), minsad(x,y)[3]),
-                                 minsad(x,y)[2], minsad(x,y)[3]});
-    Func disp("disparity");
-    disp(x, y) = FILTERED;
-    RDom ri(xmin, xmax-xmin+1, ymin, ymax-ymin+1);
-    disp(ri.x, ri.y) = select(minsad(ri.x, ri.y)[1] < minsad(ri.x,ri.y)[3] * (1+uniquenessRatio), FILTERED, minsad(ri.x, ri.y)[0]);
+    Func disp_left("disp_left");
+    disp_left(x, y) = {minDisparity, INT_MAX};
+    disp_left(x, y) = tuple_select(
+                             cSAD(x, y, rd) < disp_left(x, y)[1],
+                             {rd, cSAD(x, y, rd)},
+                             disp_left(x, y)
+                             );
 
+    // check unique match
+    Func unique("unique");
+    unique(x, y) = 1;
+    unique(x, y) = select(rd != disp_left(x, y)[0] && cSAD(x, y, rd) <= disp_left(x, y)[1] * (1 + uniquenessRatio), 0, 1);
+
+    // validate disparity by comparing left and right
+    // calculate disp2
+    Func disp_right("disp_right");
+    disp_right(x, y) = {minDisparity, INT_MAX};
+    RDom rx(xmin, xmax-xmin+1);
+    Expr x2 = clamp(rx - disp_left(rx, y)[0], xmin, xmax);
+    disp_right(x2, y) = tuple_select(
+                             disp_right(x2, y)[1] > disp_left(rx, y)[1],
+                             disp_left(rx, y),
+                             disp_right(x2, y)
+                         );
+
+    Func disp("disp");
+    x2 = clamp(x - disp_left(x, y)[0], xmin, xmax);
+    disp(x, y) = select(
+                    unique(x, y) == 0
+                    // || !(x >= xmin && x <= xmax && y >= ymin && y<= ymax)
+                    || (x2 >= xmin && x2 <= xmax && abs(disp_right(x2, y)[0] - disp_left(x, y)[0]) > disp12MaxDiff),
+                    FILTERED,
+                    disp_left(x,y)[0]
+                 );
+
+
+    // Schedule
+    // Var xi("xi"), yi("yi"), xo("xo"), yo("yo");
+    // disp.compute_root().split(y, yo, yi, 16).parallel(yo);
+    // unique.compute_inline();
+    // disp_right.compute_at(disp, yi).vectorize(x, 16);
+    // disp_left.compute_at(disp, yi).vectorize(x, 16);
+    //
+    // cSAD.compute_at(disp, yo).vectorize(x, 16).reorder(x, d, y);
+    // vsum.compute_at(cSAD, y).vectorize(d, 16).reorder(x, d, y);
+    // vsum.reorder_storage(d, x, y);
+    // diff.compute_at(cSAD, y).vectorize(d, 16).reorder(d, x, y);
+    // diff.reorder_storage(d, x, y);
     return disp;
 }
 
-// check whether machine is little endian
-int littleendian()
-{
-    int intval = 1;
-    unsigned char *uval = (unsigned char *)&intval;
-    return uval[0] == 1;
-}
 
-
-Func stereoMatch(Image<float> left_image, Image<float> right_image, int SADWindowSize, int minDisparity, int numDisparities) {
+Func stereoMatch(Image<int8_t> left_image, Image<int8_t> right_image, int SADWindowSize, int minDisparity, int numDisparities) {
     Var x("x"), y("y"), c("c");
     Func left("left"), right("right");
     left(x, y, c) = left_image(x, y, c);
     right(x, y, c) = right_image(x, y, c);
 
-    Func filteredLeft = prefilterNorm(left, 9, left_image.width(), left_image.height());
-    Func filteredRight = prefilterNorm(left, 9, right_image.width(), right_image.height());
+    int width = left_image.width();
+    int height = left_image.height();
+
+    Func filteredLeft = prefilterXSobel(left, width, height);
+    Func filteredRight = prefilterXSobel(right, width, height);
 
     /* get valid disparity region */
     int SW2 = SADWindowSize/2;
@@ -106,45 +128,16 @@ Func stereoMatch(Image<float> left_image, Image<float> right_image, int SADWindo
     Func disp = findStereoCorrespondence(filteredLeft, filteredRight, SADWindowSize, minDisparity, numDisparities,
         xmin, xmax, ymin, ymax);
 
+
+    disp.compute_root();
     apply_default_schedule(disp);
+    //
+    Target t = get_jit_target_from_environment().with_feature(Target::Profile);
+    disp.realize(width, height, t);
+    disp.compile_to_lowered_stmt("disp.html", {}, HTML);
+    // Image<int> filtered = filteredLeft.realize(width, height, 3);
+    // Halide::Tools::save_image(filtered, "filteredLeft.png");
     return disp;
-}
-
-// 1-band PFM image, see http://netpbm.sourceforge.net/doc/pfm.html
-void WriteFilePFM(Image<int> image, const char* filename, float scalefactor=1/255.0)
-{
-    // Open the file
-    FILE *stream = fopen(filename, "wb");
-    if (stream == 0) {
-        fprintf(stderr, "WriteFilePFM: could not open %s\n", filename);
-	exit(1);
-    }
-
-    // sign of scalefact indicates endianness, see pfms specs
-    if (littleendian())
-	scalefactor = -scalefactor;
-
-    int width = image.width(), height = image.height();
-
-    // write the header: 3 lines: Pf, dimensions, scale factor (negative val == little endian)
-    fprintf(stream, "Pf\n%d %d\n%f\n", width, height, scalefactor);
-
-    int n = width;
-    // write rows -- pfm stores rows in inverse order!
-    for (int y = height-1; y >= 0; y--) {
-    	float ptr[width];
-    	// change invalid pixels (which seem to be represented as -10) to INF
-    	for (int x = 0; x < width; x++) {
-    		ptr[x] = image(x,y) < 0 ? INFINITY : image(x,y);
-    	}
-    	if ((int)fwrite(ptr, sizeof(float), n, stream) != n) {
-    	    fprintf(stderr, "WriteFilePFM: problem writing data\n");
-    	    exit(1);
-    	}
-    }
-
-    // close file
-    fclose(stream);
 }
 
 #include <unistd.h>
@@ -159,7 +152,7 @@ float profile(Func myFunc, int w, int h) {
     myFunc.compile_jit();
 
     unsigned long s = millisecond_timer();
-    int N_TIMES = 10;
+    int N_TIMES = 1;
     for (int i=0; i<N_TIMES; i++) {
         myFunc.realize(w,h);
     }
@@ -170,6 +163,19 @@ float profile(Func myFunc, int w, int h) {
         << " throughput " << (mpixels*N_TIMES)/(total_time/1000) << " megapixels/sec" << std::endl;
 
     return total_time/N_TIMES;
+}
+
+CFloatImage convertHalideImageToFloatImage(Image<int> image) {
+    CFloatImage img(image.width(), image.height(), 1);
+    for (int x = 0; x < image.width(); x++) {
+        for (int y = 0; y < image.height(); y++) {
+            float* ptr = (float *) img.PixelAddress(x, y, 0);
+            *ptr = (float)image(x,y);
+            if (*ptr < 0)
+                *ptr = INFINITY;
+        }
+    }
+    return img;
 }
 
 static void print_help()
@@ -225,7 +231,7 @@ int main(int argc, char **argv) {
         else if( strncmp(argv[i], maxdisp_opt, strlen(maxdisp_opt)) == 0 )
         {
             if( sscanf( argv[i] + strlen(maxdisp_opt), "%d", &numberOfDisparities ) != 1 ||
-                numberOfDisparities < 1 || numberOfDisparities % 16 != 0 )
+                numberOfDisparities < 1 )
             {
                 printf("Command-line parameter error: The max disparity (--maxdisparity=<...>) must be a positive integer divisible by 16\n");
                 print_help();
@@ -262,14 +268,21 @@ int main(int argc, char **argv) {
         return -1;
     }
 
-    Image<float> img1 = Halide::Tools::load_image(std::string(img1_filename));
-    Image<float> img2 = Halide::Tools::load_image(std::string(img2_filename));
+    Image<int8_t> img1 = Halide::Tools::load_image(std::string(img1_filename));
+    Image<int8_t> img2 = Halide::Tools::load_image(std::string(img2_filename));
     Func disp = stereoMatch(img1, img2, SADWindowSize, 0, numberOfDisparities);
-    profile(disp, img1.width(), img1.height());
+    // profile(disp, img1.width(), img1.height());
     Image<int> disp_image = disp.realize(img1.width(), img1.height());
 
     int maxDisparity = numberOfDisparities - 1;
 
-    Halide::Tools::save_image(disp_image, std::string(disparity_filename));
-    WriteFilePFM(disp_image, disparity_filename, 1.0f/maxDisparity);
+    Image<float> scaled_disp(disp_image.width(), disp_image.height());
+    for (int y = 0; y < disp_image.height(); y++) {
+        for (int x = 0; x < disp_image.width(); x++) {
+            scaled_disp(x, y) = min(1.f, max(0.f, disp_image(x,y) * 1.0f / maxDisparity));
+        }
+    };
+
+    Halide::Tools::save_image(scaled_disp, "temp.png");
+    WriteFilePFM(convertHalideImageToFloatImage(disp_image), disparity_filename, 1.0f/maxDisparity);
 }
