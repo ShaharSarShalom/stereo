f119a5cf598c9c5dfd3a61307282d4de77f8ccaf
mzhang94 <mzhang94@mit.edu>
2016-05-13 04:43:09 -0400

    add GPU schedule for stereoBM

--- StereoHalide/lib/stereoBM.cpp
+++ StereoHalide/lib/stereoBM.cpp
@@ -6,7 +6,7 @@
 using namespace Halide;
 short FILTERED = -16;
 
-Func prefilterXSobel(Func image, int w, int h) {
+Func prefilterXSobel(Func image, int w, int h, bool schedule_GPU) {
     Var x("x"), y("y");
     Func clamped("clamped"), gray("gray");
     gray(x, y) = 0.2989f*image(x, y, 0) + 0.5870f*image(x, y, 1) + 0.1140f*image(x, y, 2);
@@ -17,15 +17,24 @@ Func prefilterXSobel(Func image, int w, int h) {
     xSobel(x, y) = cast<short>(clamp(temp(x, y-1) + 2 * temp(x, y) + temp(x, y+1), -31, 31));
 
     // Schedule
-    Var xi("xi"), xo("xo"), yi("yi"), yo("yo");
-    xSobel.compute_root().tile(x, y, xo, yo, xi, yi, 64, 32).parallel(yo).parallel(xo);
-    temp.compute_at(xSobel, yi).vectorize(x, 8);
+    if (!schedule_GPU)
+    {
+        Var xi("xi"), xo("xo"), yi("yi"), yo("yo");
+        xSobel.compute_root().tile(x, y, xo, yo, xi, yi, 64, 32).parallel(yo).parallel(xo);
+        temp.compute_at(xSobel, yi).vectorize(x, 8);
+    }
+    else
+    {
+        Var xi("xi"), xo("xo"), yi("yi"), yo("yo");
+        xSobel.compute_root().gpu_tile(x, y, 16, 16);
+        temp.compute_at(xSobel, Var::gpu_blocks());
+    }
     return xSobel;
 }
 
 Func findStereoCorrespondence(Func left, Func right, int SADWindowSize, int minDisparity, int numDisparities,
     int width, int height, int xmin, int xmax, int ymin, int ymax,
-    int x_tile_size = 32, int y_tile_size = 32, bool test = false, float uniquenessRatio = 0.15, int disp12MaxDiff = 1) {
+    int x_tile_size = 32, int y_tile_size = 32, bool useGPU = false, float uniquenessRatio = 0.15, int disp12MaxDiff = 1) {
 
     Var x("x"), y("y"), c("c"), d("d");
 
@@ -41,9 +50,26 @@ Func findStereoCorrespondence(Func left, Func right, int SADWindowSize, int minD
     Func cSAD("cSAD"), vsum("vsum");
     RDom rk(-win2, SADWindowSize, "rk");
     RDom rxi(1, x_tile_size - 1, "rxi"), ryi(1, y_tile_size - 1, "ryi");
-    if (test) {
-        vsum(d, xi, yi, xo, yo) = sum(diff_T(d, xi, yi+rk, xo, yo));
+    RDom rd(minDisparity, numDisparities);
+    Func disp_left("disp_left");
+    Func disp("disp");
+    if (useGPU) {
+        vsum(d, xi, yi, xo, yo) =  sum(diff_T(d, xi, yi+rk, xo, yo));
         cSAD(d, xi, yi, xo, yo) = sum(vsum(d, xi+rk, yi, xo, yo));
+
+        // vsum(d, x, y) = sum(diff(d, x, y+rk));
+        // cSAD(d, x, y) = sum(vsum(d, x+rk, y));
+        //
+        // disp_left(x, y) = {cast<ushort>(minDisparity), cast<ushort>((2<<16)-1)};
+        // disp_left(x, y) = tuple_select(
+        //         cSAD(rd, x, y) < disp_left(x, y)[1],
+        //         {cast<ushort>(rd), cSAD(rd, x, y)},
+        //         disp_left(x, y));
+        //
+        // disp(x, y) = select(
+        //     x>xmax-xmin || y>ymax-ymin,
+        //     cast<ushort>(FILTERED),
+        //     cast<ushort>(disp_left(x, y)[0]));
     }
     else{
         vsum(d, xi, yi, xo, yo) = select(yi != 0, cast<ushort>(0), sum(diff_T(d, xi, rk, xo, yo)));
@@ -52,51 +78,77 @@ Func findStereoCorrespondence(Func left, Func right, int SADWindowSize, int minD
         cSAD(d, xi, yi, xo, yo) = sum(vsum(d, xi+rk, yi, xo, yo));
     }
 
-    RDom rd(minDisparity, numDisparities);
-    Func disp_left("disp_left");
     disp_left(xi, yi, xo, yo) = {cast<ushort>(minDisparity), cast<ushort>((2<<16)-1)};
     disp_left(xi, yi, xo, yo) = tuple_select(
-            cSAD(rd, xi, yi, xo, yo) < disp_left(xi, yi, xo, yo)[1],
-            {cast<ushort>(rd), cSAD(rd, xi, yi, xo, yo)},
-            disp_left(xi, yi, xo, yo));
+        cSAD(rd, xi, yi, xo, yo) < disp_left(xi, yi, xo, yo)[1],
+        {cast<ushort>(rd), cSAD(rd, xi, yi, xo, yo)},
+    disp_left(xi, yi, xo, yo));
 
-    Func disp("disp");
     disp(x, y) = select(
-        x>xmax-xmin || y>ymax-ymin,
-        cast<ushort>(FILTERED),
-        cast<ushort>(disp_left(x%x_tile_size, y%y_tile_size, x/x_tile_size, y/y_tile_size)[0]));
+            x>xmax-xmin || y>ymax-ymin,
+            cast<ushort>(FILTERED),
+            cast<ushort>(disp_left(x%x_tile_size, y%y_tile_size, x/x_tile_size, y/y_tile_size)[0]));
+
 
     int vector_width = 8;
 
     // Schedule
-    disp.compute_root().tile(x, y, xo, yo, xi, yi, x_tile_size, y_tile_size).reorder(xi, yi, xo, yo)
+    if (useGPU)
+    {
+        Var x_thread, y_thread;
+        // disp.compute_root().tile(x, y, xo, yo, xi, yi, x_tile_size, y_tile_size)
+        //     .tile(xi, yi, x_thread, y_thread, xi, yi, x_tile_size/16, y_tile_size/16)
+        //     .gpu_blocks(xo, yo).gpu_threads(x_thread, y_thread);
+        disp.compute_root().tile(x, y, xo, yo, xi, yi, x_tile_size, y_tile_size)
+            .gpu_blocks(xo, yo).gpu_threads(xi, yi);
+
+        // reorder storage
+        disp_left.reorder_storage(xi, yi, xo, yo);
+        diff_T   .reorder_storage(xi, yi, xo, yo, d);
+        vsum     .reorder_storage(xi, yi, xo, yo, d);
+        cSAD     .reorder_storage(xi, yi, xo, yo, d);
+
+        disp_left.compute_at(disp, Var::gpu_blocks()).gpu_threads(xi, yi)
+                 .update().reorder(xi, yi, xo, yo, rd).gpu_threads(xi, yi);
+
+        cSAD.compute_at(disp_left, rd).gpu_threads(xi, yi);
+        vsum.compute_at(disp_left, rd).gpu_threads(xi, yi);
+
+        // disp_left.compute_at(disp, Var::gpu_blocks()).tile(xi, yi, x_thread, y_thread, xi, yi, x_tile_size/16, y_tile_size/16).gpu_threads(x_thread, y_thread)
+        //          .update().tile(xi, yi, x_thread, y_thread, xi, yi, x_tile_size/16, y_tile_size/16).reorder(xi, yi, x_thread, y_thread, xo, yo, rd).gpu_threads(x_thread, y_thread);
+        //
+        // cSAD.compute_at(disp_left, rd).tile(xi, yi, x_thread, y_thread, xi, yi, x_tile_size/16, y_tile_size/16).gpu_threads(x_thread, y_thread);
+        // vsum.compute_at(disp_left, rd).tile(xi, yi, x_thread, y_thread, xi, yi, x_tile_size/16, y_tile_size/16).gpu_threads(x_thread, y_thread);
+        Target target = get_host_target();
+        target.set_feature(Target::OpenCL);
+        disp.compile_jit(target);
+        disp.compile_to_lowered_stmt("disp.html", {}, HTML, target);
+    }
+    else
+    {
+        disp.compute_root().tile(x, y, xo, yo, xi, yi, x_tile_size, y_tile_size).reorder(xi, yi, xo, yo)
         .vectorize(xi, vector_width).parallel(xo).parallel(yo);
 
-    // reorder storage
-    disp_left.reorder_storage(xi, yi, xo, yo);
-    diff_T   .reorder_storage(xi, yi, xo, yo, d);
-    vsum     .reorder_storage(xi, yi, xo, yo, d);
-    cSAD     .reorder_storage(xi, yi, xo, yo, d);
+        // reorder storage
+        disp_left.reorder_storage(xi, yi, xo, yo);
+        diff_T   .reorder_storage(xi, yi, xo, yo, d);
+        vsum     .reorder_storage(xi, yi, xo, yo, d);
+        cSAD     .reorder_storage(xi, yi, xo, yo, d);
 
-    disp_left.compute_at(disp, xo).reorder(xi, yi, xo, yo)    .vectorize(xi, vector_width)
-             .update()            .reorder(xi, yi, rd, xo, yo).vectorize(xi, vector_width);
+        disp_left.compute_at(disp, xo).reorder(xi, yi, xo, yo)    .vectorize(xi, vector_width)
+        .update()            .reorder(xi, yi, rd, xo, yo).vectorize(xi, vector_width);
 
-    if (test){
-        cSAD.compute_at(disp_left, rd).reorder(xi,  yi, xo, yo, d).vectorize(xi, vector_width);
-        vsum.compute_at(disp_left, rd).reorder(xi,  yi, xo, yo, d).vectorize(xi, vector_width);
-    }
-    else {
         cSAD.compute_at(disp_left, rd).reorder(xi,  yi, xo, yo, d).vectorize(xi, vector_width);
         vsum.compute_at(disp_left, rd).reorder(xi,  yi, xo, yo, d).vectorize(xi, vector_width)
-            .update()                 .reorder(xi, ryi, xo, yo, d).vectorize(xi, vector_width);
+        .update()                 .reorder(xi, ryi, xo, yo, d).vectorize(xi, vector_width);
+        disp.compile_to_lowered_stmt("disp.html", {}, HTML);
     }
 
     return disp;
 }
 
-
 Image<ushort> stereoBM(Image<uint8_t> left_image, Image<uint8_t> right_image, int SADWindowSize, int minDisparity,
-              int numDisparities, int xmin, int xmax, int ymin, int ymax) {
+              int numDisparities, int xmin, int xmax, int ymin, int ymax, bool useGPU) {
     Var x("x"), y("y"), c("c");
     Func left("left"), right("right");
     left(x, y, c) = left_image(x, y, c);
@@ -105,20 +157,19 @@ Image<ushort> stereoBM(Image<uint8_t> left_image, Image<uint8_t> right_image, in
     int width = left_image.width();
     int height = left_image.height();
 
-    Func filteredLeft = prefilterXSobel(left, width, height);
-    Func filteredRight = prefilterXSobel(right, width, height);
+    Func filteredLeft = prefilterXSobel(left, width, height, useGPU);
+    Func filteredRight = prefilterXSobel(right, width, height, useGPU);
 
-    int x_tile_size = 64, y_tile_size = 32;
+    int x_tile_size = 16, y_tile_size = 16;
     Func disp = findStereoCorrespondence(filteredLeft, filteredRight, SADWindowSize, minDisparity, numDisparities,
-        left_image.width(), left_image.height(), xmin, xmax, ymin, ymax, x_tile_size, y_tile_size);
-    disp.compile_to_lowered_stmt("disp.html", {}, HTML);
+        left_image.width(), left_image.height(), xmin, xmax, ymin, ymax, x_tile_size, y_tile_size, useGPU);
 
     int w = (xmax-xmin)/x_tile_size*x_tile_size+x_tile_size;
     int h = (ymax-ymin)/x_tile_size*x_tile_size+x_tile_size;
 
-    profile(disp, w, h, 100);
-    Target t = get_jit_target_from_environment().with_feature(Target::Profile);
-    Image<ushort> disp_image = disp.realize(w, h, t);
+    Image<ushort> disp_image(w, h);
+    disp.realize(disp_image);
 
+    profile(disp, disp_image, 100);
     return disp_image;
 }
